e424ad4a773e9bed5f40cfa05c367c3f803fcc34
lookfiresu123 <lookfiresu123@gmail.com>
2016-12-26 13:55:07 +0800

    everything is ok except __tracepoint_ext4_journal_start() undefined

--- share/open.c
+++ share/open.c
@@ -87,61 +87,6 @@ static LLIST_HEAD(delayed_fput_list);
 static DEFINE_MUTEX(epmutex);
 DEFINE_SEQLOCK(mount_lock);
 
-/*----------------------------  ext4 文件系统层 -------------------------------*/
-static int my_ext4_file_open(struct inode * inode, struct file * filp, struct task_struct *t)
-{
-  DEBUG_LOG("entry my_ext4_file_open()!");
-	struct super_block *sb = inode->i_sb;
-	struct ext4_sb_info *sbi = EXT4_SB(inode->i_sb);
-	struct vfsmount *mnt = filp->f_path.mnt;
-	struct path path;
-	char buf[64], *cp;
-  printk("FILE = %s, LINE = %d, FUNC = %s, current = %p, current->files = %p\n", __FILE__, __LINE__, __FUNCTION__, t, t->files);
-
-	if (unlikely(!(sbi->s_mount_flags & EXT4_MF_MNTDIR_SAMPLED) && !(sb->s_flags & MS_RDONLY))) {
-		sbi->s_mount_flags |= EXT4_MF_MNTDIR_SAMPLED;
-		/*
-		 * Sample where the filesystem has been mounted and
-		 * store it in the superblock for sysadmin convenience
-		 * when trying to sort through large numbers of block
-		 * devices or filesystem images.
-		 */
-    printk("FILE = %s, LINE = %d, FUNC = %s, current = %p, current->files = %p\n", __FILE__, __LINE__, __FUNCTION__, t, t->files);
-		memset(buf, 0, sizeof(buf));
-		path.mnt = mnt;
-		path.dentry = mnt->mnt_root;
-		cp = d_path(&path, buf, sizeof(buf));
-		if (!IS_ERR(cp)) {
-			handle_t *handle;
-			int err;
-
-			handle = ext4_journal_start_sb(sb, EXT4_HT_MISC, 1);
-			if (IS_ERR(handle))
-				return PTR_ERR(handle);
-			err = ext4_journal_get_write_access(handle, sbi->s_sbh);
-			if (err) {
-				ext4_journal_stop(handle);
-				return err;
-			}
-			strlcpy(sbi->s_es->s_last_mounted, cp, sizeof(sbi->s_es->s_last_mounted));
-			ext4_handle_dirty_super(handle, sb);
-			ext4_journal_stop(handle);
-		}
-	}
-  printk("FILE = %s, LINE = %d, FUNC = %s, current = %p, current->files = %p\n", __FILE__, __LINE__, __FUNCTION__, t, t->files);
-	/*
-	 * Set up the jbd2_inode if we are opening the inode for
-	 * writing and the journal is present
-	 */
-	if (filp->f_mode & FMODE_WRITE) {
-		int ret = ext4_inode_attach_jinode(inode);
-		if (ret < 0)
-			return ret;
-	}
-  printk("FILE = %s, LINE = %d, FUNC = %s, current = %p, current->files = %p\n", __FILE__, __LINE__, __FUNCTION__, t, t->files);
-	return dquot_file_open(inode, filp);
-}
-
 
 /* ------------------      新加的函数（虚拟文件系统层）      ----------------------- */
 static inline void mnt_inc_writers(struct mount *mnt)
